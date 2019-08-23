d70f2dc11a161b7667b83d3ab7290be732b35b99
JesisWang <baobaolenren777@sina.com>
2019-08-23 08:07:53 +0800

    hello

---
+++ Pythonlearns/Text/Text1/__init__.py
@@ -0,0 +1,22 @@
+# from Tools.scripts.treesync import raw_input
+# name=raw_input('what is your name:')
+# # name='12tank'
+# # print(name)
+# print(type(name))
+# if name.endswith('tank'):
+#     print('Hello tank')
+# elif name.endswith('xiao'):
+#     print('Hello xiao')
+# else:
+#     print('Hello Strange')
+# from test.datetimetester import DAY
+# day=[1,2,3,4,5]
+# print(day)
+# length=len(day)
+# print(length)
+# index=length-2
+# lastvalue=day[2:4]
+# print(lastvalue)
+# # print(day[-2])
+# for d in day:
+#     print(d)
